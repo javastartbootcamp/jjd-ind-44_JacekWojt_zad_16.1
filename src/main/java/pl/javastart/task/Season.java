8114779e8c0d0adc566a935add09cb8c4338aeb4
Jacek Wojtuszek <jacek.wojtuszek@gmail.com>
2023-03-02 00:01:56 +0100

    Rozwiązanie

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -1,5 +1,40 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
+
 public enum Season {
-    //uzupełnij
+    SPRING("Wiosna", new String[]{"marzec", "kwiecień", "maj"}),
+    SUMMER("Lato", new String[]{"czerwiec", "lipiec", "sierpień"}),
+    AUTUMN("Jesień", new String[]{"wrzesień", "październik", "listopad"}),
+    WINTER("Zima", new String[]{"grudzień", "styczeń", "luty"});
+
+    private final String plSeason;
+    private final String[] seasonMonths;
+
+    Season(String plSeason, String[] seasonMonths) {
+        this.plSeason = plSeason;
+        this.seasonMonths = seasonMonths;
+    }
+
+    public String plTranslation() {
+        return plSeason;
+    }
+
+    public static void showSeasonMonths(String season) {
+        System.out.println("W tej porze roku są następujące miesiące:");
+        switch (season) {
+            case "wiosna":
+                System.out.println(Arrays.toString(SPRING.seasonMonths));
+                break;
+            case "lato":
+                System.out.println(Arrays.toString(SUMMER.seasonMonths));
+                break;
+            case "jesień":
+                System.out.println(Arrays.toString(AUTUMN.seasonMonths));
+                break;
+            default:
+                System.out.println(Arrays.toString(WINTER.seasonMonths));
+                break;
+        }
+    }
 }
--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -1,12 +1,17 @@
 package pl.javastart.task;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class SeasonManager {
 
-    // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
-
+        Season[] seasons = Season.values();
+        System.out.println("Podaj pore roku:");
+        for (Season season : seasons) {
+            System.out.println(season.plTranslation());
+        }
+        String season = scanner.nextLine().toLowerCase(Locale.ROOT);
+        Season.showSeasonMonths(season);
     }
-
 }
